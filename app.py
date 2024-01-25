0955d108fab74d6da4c68eacd6dc2aadd1d68c57
raaasin <nisarvskp@gmail.com>
2024-01-25 14:54:38 +0530

    version alpha

--- app.py
+++ app.py
@@ -13,14 +13,14 @@ def get_message(message):
     response.resolve()
     print(response.text)
     return response.text
-
+flag = 0
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         cnc_name = request.form['cnc_name']
         material = request.form['material']
         additional_criteria = request.form['additional_criteria']
-        message = f"{cnc_name} for {material}, with {additional_criteria} help me find one perfect CNC machine for this purpose."
+        message = f"{cnc_name} for {material}, {additional_criteria} help me find a perfect CNC machine for this purpose, make sure to mention only one cnc machine and give 2 lines description about it thats it nothing else."
         return render_template('result.html', initiate=message)
 
     return render_template('index.html')
@@ -28,8 +28,10 @@ def index():
 @app.route('/send_message', methods=['POST'])
 def send_message():
     user_message = request.form['message']
+    print("recieved",user_message)
     chat_response = get_message(user_message)  
     return jsonify({'response': chat_response})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000)
+
