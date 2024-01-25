8b0a963526a12e544e51bb66d280471da5494cd2
raaasin <nisarvskp@gmail.com>
2024-01-25 13:15:58 +0530

    check-point

--- app.py
+++ app.py
@@ -1,41 +1,35 @@
 from flask import Flask, render_template, request
 import google.generativeai as genai
-
+from flask import jsonify
 app = Flask(__name__)
 
 genai.configure(api_key="AIzaSyDWflxCyU3_pSzXjW-k91I9G6mvzUQ2q2U")
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
+
 
 def get_message(message):
-    model = genai.GenerativeModel('gemini-pro')
-    chat = model.start_chat(history=[])
     response = chat.send_message(message, stream=True)
     response.resolve()
-    return response
+    print(response.text)
+    return response.text
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Retrieve form data
         cnc_name = request.form['cnc_name']
         material = request.form['material']
         additional_criteria = request.form['additional_criteria']
-
-        # Build the message for the API call
-        message = f"i want to use it at {cnc_name} for this operation ${material}, with additional criteria ${additional_criteria} just provide the name of one single cnc thats it nothing else, provide answer without formatting, just plain text."
-        answer = get_message(message)
-        
-        newmess=f"Describe about The CNC machine '{answer.text}' in a few short lines wihout formatting just plain text"
-        about=get_message(newmess)
-        abt=""
-        for i in about.text:
-            if i=='\n':
-                abt+=" "
-            else:
-                abt+=i
-
-        return render_template('result.html', answer=answer, about=abt)
+        message = f"{cnc_name} for {material}, with {additional_criteria} help me find one perfect CNC machine for this purpose."
+        return render_template('result.html', initiate=message)
 
     return render_template('index.html')
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    user_message = request.form['message']
+    chat_response = get_message(user_message)  
+    return jsonify({'response': chat_response})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000)
