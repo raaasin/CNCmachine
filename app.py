5eb246fcd73ff90692080670f2dd13380b783c2c
raaasin <nisarvskp@gmail.com>
2024-01-31 19:33:46 +0530

    format

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ def index():
         additional_criteria = request.form['additional_criteria']
         message = f"Pretend to be a CNC machine expert/chatbot,now answer this, {cnc_name} for {material}, {additional_criteria} help me find a perfect CNC machine for this purpose, make sure to mention only one cnc machine and give 2 lines description about it thats it nothing else. ALso remember you can only answer related to CNC's and nothing else."
         return render_template('result.html', initiate=message)
-
     return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000)
 
